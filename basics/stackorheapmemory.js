867edf9e4b9bc7217eb580434960ca1cf800a9b2
VY25AY <133554801+VY25AY@users.noreply.github.com>
2024-11-25 18:52:01 +0000

    commit

---
+++ basics/stackorheapmemory.js
@@ -0,0 +1,18 @@
+// stack(primitive value)
+//heap(Non-primitive)
+let myname="Vaishnavi"
+let anothername=myname
+anothername="purva"
+console.log(myname);
+console.log(anothername);
+
+
+
+let userone={
+    email:"ghh@gmail.com",
+    upi:"user@paytm"
+}
+let userTwo=userone
+userTwo.email="v@google.com"
+console.log(userone.email);
+console.log(userTwo.email);
---
+++ basics/string.js
@@ -0,0 +1,22 @@
+// denoted by ""or ''
+const name="Vaishnavi"
+const repocount=5
+console.log(name+repocount+"Value"); 
+console.log(`my name is ${name} and my repo count is ${repocount} `);
+const getName=new String('Purva');
+console.log(getName[3]);
+console.log(getname.__proto__);
+console.log(getName.length);
+console.log(getname.toUpperCase());
+console.log(getname.charAt(3));
+console.log(getname.indexof('u'));
+const newString=getname.substring(0,4);
+console.log(newString);
+
+
+const anotherstring=getname.slice(-8,6);
+console.log(anotherstring);
+
+
+const newStringo="    purva   ";
+console.log(newStringo.trim());
